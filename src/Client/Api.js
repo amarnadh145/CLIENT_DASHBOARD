61e84f883200889ae42861da3f8343c7a6441260
Amarnadh Reddy Mettu <amarnadhusa@gmail.com>
2025-02-11 19:00:35 -0500

    Update Api.js

--- src/Client/Api.js
+++ src/Client/Api.js
@@ -1,2 +1,2 @@
 // export const API_URL = "http://localhost:4000"
-export const API_URL = 'https://backend-nodejs-restaurant.onrender.com'
+export const API_URL = "https://backend-nodejs-restaurant.onrender.com"
